a2169d281a6e33d432df5c52d99f9c93f119b082
Maxime BRODIN <mxb@MacBook-Pro-de-Maxime.local>
2025-09-17 14:32:36 +0200

    Ajout & update de l'essai

--- TP7/mastermind.py
+++ TP7/mastermind.py
@@ -10,3 +10,22 @@ class Mastermind:
 
     def generercode(self):
         return [random.choice(self.couleurs) for _ in range(self.taille)]
+
+    def ajoutessai(self, essai):
+        self.essais.append(essai)
+
+    def verifessai(self, essai):
+        bon = 0
+        mauvais = 0
+        code = self.codesecret.copy()
+        essai = essai.copy()
+        for i in range(self.taille):
+            if essai[i] == code[i]:
+                bon += 1
+                code[i] = None
+                essai[i] = "_"
+        for i in range(self.taille):
+            if essai[i] in code and essai[i] != "_":
+                mauvais += 1
+                code[code.index(essai[i])] = None
+        return bon, mauvais
